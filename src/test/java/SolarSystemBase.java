86ac7cff88029270a789389c69e6187574259678
Daniel03031990 <grachiladaniel@gmail.com>
2024-11-20 11:11:16 +0000

    added planet2

--- src/test/java/SolarSystemBase.java
+++ src/test/java/SolarSystemBase.java
@@ -1,18 +1,23 @@
 public class SolarSystemBase {
     public static void main(String[] args) {
 
-        BasicSystem basicSystem=new BasicSystem();
+        BasicSystem basicSystem = new BasicSystem();
 
-        basicSystem.color="Red";
-        basicSystem.latitude="23534254523";
-        basicSystem.miles="654646464";
-        basicSystem.distance="13636456";
+        basicSystem.color = "Red";
+        basicSystem.latitude = "23534254523";
+        basicSystem.miles = "654646464";
+        basicSystem.distance = "13636456";
 
         System.out.println(basicSystem.color + " " + basicSystem.distance);
-        featureSun featureSun=new featureSun();
+        featureSun featureSun = new featureSun();
 
-        featureSun.speed=1234;
-        featureSun.time=4;
-        featureSun.year=2025;
+        featureSun.speed = 1234;
+        featureSun.time = 4;
+        featureSun.year = "Saturn";
+
+        planet2 planet2 = new planet2();
+        planet2.availability = "always";
+        planet2.year="Earth";
+        planet2.quantity="516";
     }
 }
