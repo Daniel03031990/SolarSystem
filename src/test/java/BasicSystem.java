9bdd3b297f42458accc46275105de45a8410b0e4
Daniel03031990 <grachiladaniel@gmail.com>
2024-11-20 09:44:14 +0000

    BasicSystem

---
+++ src/test/java/BasicSystem.java
@@ -0,0 +1,6 @@
+public class BasicSystem {
+    String color;
+    String distance;
+    String latitude;
+    String miles;
+}
---
+++ src/test/java/SolarSystemBase.java
@@ -0,0 +1,13 @@
+public class SolarSystemBase {
+    public static void main(String[] args) {
+
+        BasicSystem basicSystem=new BasicSystem();
+
+        basicSystem.color="Red";
+        basicSystem.latitude="23534254523";
+        basicSystem.miles="654646464";
+        basicSystem.distance="13636456";
+
+        System.out.println(basicSystem.color + " " + basicSystem.distance);
+    }
+}
